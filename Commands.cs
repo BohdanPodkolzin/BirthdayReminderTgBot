65d808ff93831c0bd0ad1c01aee1aef5791e02a1
szymptom <bpodkolzin@kse.org.ua>
2024-01-07 05:37:21 +0200

    first commit

---
+++ Commands.cs
@@ -0,0 +1,41 @@
+﻿using PRTelegramBot.Attributes;
+using PRTelegramBot.Core;
+using PRTelegramBot.Models;
+using PRTelegramBot.Utils;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace tg
+{
+    public class Commands
+    {
+        [ReplyMenuHandler("/start")]
+        public static async Task StartBot(ITelegramBotClient botClient, Update update)
+        {   
+            var startMessage = $"Get ready, to do list of bithdays enter /menu";
+            var printStartedMessage = await PRTelegramBot.Helpers.Message.Send(botClient, update, startMessage);
+            // var firstMessage = $"Hello, {update.Message.From.FirstName}!\n This bot was created by @szymptom like first project. This creation is aimed at helping with the schedule of the day of births";
+            // var printFirstMessage = await PRTelegramBot.Helpers.Message.Send(botClient, update, firstMessage);
+
+        }
+
+
+        [ReplyMenuHandler("/menu")]
+        public static async Task Menu(ITelegramBotClient botClient, Update update)
+        {   
+            string menuMessage = $"All opportunities of this bot";
+            
+            List<KeyboardButton> menuList = new List<KeyboardButton>();
+            menuList.Add(new KeyboardButton("About"));
+            menuList.Add(new KeyboardButton("Edit Countdown"));
+
+            ReplyKeyboardMarkup menu = MenuGenerator.ReplyKeyboard(2, menuList);
+            OptionMessage option = new OptionMessage();
+            option.MenuReplyKeyboardMarkup = menu;
+
+            Message showMenu = await PRTelegramBot.Helpers.Message.Send(botClient, update, menuMessage, option);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using PRTelegramBot;
+using PRTelegramBot.Core;
+using tg;
+
+const string EXIT = "exit";
+
+
+var tgBot = new PRBot(option =>
+{
+    option.Token = BotCfg.Token;
+    option.Admins = new List<long> { };
+    option.WhiteListUsers = new List<long> { };
+    option.ClearUpdatesOnStart = true;
+    option.BotId = 0;
+});
+
+tgBot.OnLogCommon += TgBot_OnLogCommon;
+tgBot.OnLogError += TgBot_OnLogError;
+
+await tgBot.Start();
+static void TgBot_OnLogError(Exception ex, long? chatId)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    string errorMsg = $"{DateTime.Now}:{ex.Message}";
+    Console.WriteLine(errorMsg);
+    Console.ResetColor();
+}
+
+static void TgBot_OnLogCommon(string msg, PRBot.TelegramEvents typeEvent, ConsoleColor color)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    string commonMsg = $"{DateTime.Now}:{msg}";
+    Console.WriteLine(commonMsg);
+    Console.ResetColor();
+}
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result?.ToLower() == EXIT)
+    {
+        Environment.Exit(0);
+    }
+}
