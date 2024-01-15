45d933f3e6a30a35ef69d21e304e40c40c98c69f
szymptom <bpodkolzin@kse.org.ua>
2024-01-16 01:13:29 +0200

    step-by-step logic

--- StepCommand.cs
+++ StepCommand.cs
@@ -1,6 +1,7 @@
 ﻿using PRTelegramBot.Attributes;
 using PRTelegramBot.Extensions;
 using PRTelegramBot.Models;
+using PRTelegramBot.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,30 @@ namespace tg
         {
             string msg = "Write person`s name";
 
-            update.RegisterStepHandler(new StepTelegram(StepOne, new UserCache()));
+            update.RegisterStepHandler(new StepTelegram(StepOne, new UserCacheTemporary()));
             Message _ = await PRTelegramBot.Helpers.Message.Send(botClient, update, msg);
         }
 
         public static async Task StepOne(ITelegramBotClient botClient, Update update)
         {
+            string message = $"Testing: {update.Message?.Text}\nEnterbirthdayDate";
 
+            var handler = update.GetStepHandler<StepTelegram>();
+            handler!.GetCache<UserCacheTemporary>().PersonName = update.Message?.Text;
+
+            handler.RegisterNextStep(StepTwo, DateTime.Now.AddMinutes(8));
+
+            Message _ = await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+        }
+
+        public static async Task StepTwo(ITelegramBotClient botClient, Update update)
+        {
+            string message = $"TestingStepTwo: {update.Message?.Text}";
+            var handler = update.GetStepHandler<StepTelegram>();
+            handler!.GetCache<UserCacheTemporary>().Date = update.Message?.Text;
+
+            Message _ = await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+            update.ClearStepUserHandler();
         }
     }
 }
---
+++ UserCacheTemporary.cs
@@ -0,0 +1,17 @@
+﻿using PRTelegramBot.Models;
+using PRTelegramBot.Interface;
+
+namespace tg
+{
+    public class UserCacheTemporary : ITelegramCache
+    {
+        public string? Date { get; set; }
+        public string? PersonName { get; set; }
+        public bool ClearData()
+        {
+            this.PersonName = string.Empty;
+            this.Date = string.Empty;
+            return true;
+        }
+    }
+}
