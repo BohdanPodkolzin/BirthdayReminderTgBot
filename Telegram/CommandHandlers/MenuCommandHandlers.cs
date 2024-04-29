d17fe42b060ceb1cb09973e245713cde6df27142
Bardin08 <vbardin810@gmail.com>
2024-04-29 21:56:28 +0300

    Refactor MenuCommandHandlers.cs

--- Telegram/CommandHandlers/MenuCommandHandlers.cs
+++ Telegram/CommandHandlers/MenuCommandHandlers.cs
@@ -1,11 +1,9 @@
 ﻿using BirthdayReminder.Telegram.Helpers;
 using PRTelegramBot.Attributes;
 using PRTelegramBot.Extensions;
-using PRTelegramBot.Models;
-using PRTelegramBot.Utils;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.ReplyMarkups;
+using TgMessageHelper = PRTelegramBot.Helpers.Message;
 
 namespace BirthdayReminder.Telegram.CommandHandlers
 {
@@ -17,21 +15,8 @@ namespace BirthdayReminder.Telegram.CommandHandlers
         public static async Task Menu(ITelegramBotClient botClient, Update update)
         {
             const string menuMessage = "Homepage🏡";
-
-            List<KeyboardButton> menuList =
-            [
-                new KeyboardButton("About"),
-                new KeyboardButton("Edit Countdown"),
-                new KeyboardButton("Show countdown")
-            ];
-
-            var menu = MenuGenerator.ReplyKeyboard(2, menuList);
-            var option = new OptionMessage
-            {
-                MenuReplyKeyboardMarkup = menu
-            };
-
-            await PRTelegramBot.Helpers.Message.Send(botClient, update, menuMessage, option);
+            var option = ReplyKeyboardHelper.GetBotMenu().AsOption();
+            await TgMessageHelper.Send(botClient, update, menuMessage, option);
         }
 
         [ReplyMenuHandler("About")]
@@ -39,30 +24,29 @@ namespace BirthdayReminder.Telegram.CommandHandlers
         {
             const string message = "This bot was created by @szymptom like first project.\n" +
                                    "This creation is aimed at helping with the schedule of the day of births";
-            await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+            await TgMessageHelper.Send(botClient, update, message);
         }
 
         [ReplyMenuHandler("Edit Countdown")]
         public static async Task EditCountdown(ITelegramBotClient botClient, Update update)
         {
             var chatId = update.GetChatId();
+            await RemoveBotDuplicateMessage(botClient, chatId);
 
+            const string message = "Editing Schedule";
+            var option = InlineKeyboardsHelper.CountdownMenuKeyboard().AsOption();
+            var sendMessage = await TgMessageHelper.Send(botClient, update, message, option);
+
+            SavePrevMessageIdInChat(chatId, sendMessage.MessageId);
+        }
+
+        private static async Task RemoveBotDuplicateMessage(ITelegramBotClient botClient, long chatId)
+        {
             var prevMessageId = GetPrevMessageIdInChat(chatId);
             if (prevMessageId != -1)
             {
                 await botClient.DeleteMessageAsync(chatId, prevMessageId);
             }
-
-            var editorMenu = InlineKeyboardsHelper.CountdownMenuKeyboard();
-            var option = new OptionMessage
-            {
-                MenuInlineKeyboardMarkup = editorMenu
-            };
-
-            const string message = "Editing Schedule";
-            var sendMessage = await PRTelegramBot.Helpers.Message.Send(botClient, update, message, option);
-            
-            SavePrevMessageIdInChat(chatId, sendMessage.MessageId);
         }
 
         public static int GetPrevMessageIdInChat(long chatId)
---
+++ Telegram/Helpers/ReplyKeyboardHelper.cs
@@ -0,0 +1,23 @@
+using PRTelegramBot.Models;
+using PRTelegramBot.Utils;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BirthdayReminder.Telegram.Helpers;
+
+public static class ReplyKeyboardHelper
+{
+    public static ReplyKeyboardMarkup GetBotMenu()
+    {
+        List<KeyboardButton> menuList =
+        [
+            new KeyboardButton("About"),
+            new KeyboardButton("Edit Countdown"),
+            new KeyboardButton("Show countdown")
+        ];
+
+        return MenuGenerator.ReplyKeyboard(2, menuList);
+    }
+
+    public static OptionMessage AsOption(this ReplyKeyboardMarkup keyboardMarkup)
+        => new() { MenuReplyKeyboardMarkup = keyboardMarkup };
+}
