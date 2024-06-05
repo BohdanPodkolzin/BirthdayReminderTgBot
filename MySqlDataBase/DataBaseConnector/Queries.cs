c07b1e984808422092d98f406a804c6edf0b3989
szymptom <bpodkolzin@kse.org.ua>
2024-06-05 19:35:09 +0300

    Refactored MySqlDataBase/ folder

---
+++ MySqlDataBase/DataBaseConnector/Queries.cs
@@ -0,0 +1,187 @@
+﻿using BirthdayReminder.DependencyInjectionConfiguration;
+using BirthdayReminder.MySqlDataBase.DataBaseConnector;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using Telegram.Bot.Types;
+
+namespace BirthdayReminder.DataBase.DataBaseConnector
+{
+    public static class Queries
+    {
+        private static readonly string? ConnectionString = BotConfiguration.GetConnectionString();
+
+        public static async Task ReadAllRecords()
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection, "SELECT * FROM users_schedule");
+
+            await using (var reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    Console.WriteLine(
+                        "Reading from table=({0}, {1}, {2}, {3:dd.MM.yyyy})",
+                        reader.GetInt32(0),
+                        reader.GetInt64(1),
+                        reader.GetString(2),
+                        reader.IsDBNull(3)
+                            ? DateTime.MinValue
+                            : reader.GetDateTime(3)
+                    );
+                }
+            }
+        }
+
+        public static async Task InsertRecordByNameAndDate(long userId, string personName, DateTime date)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "INSERT INTO users_schedule (user_telegram_id, human_in_schedule, bday_date) VALUES (@userId, @personName, @date)");
+            
+            AddParametersByInput(command, 1, userId, personName, date);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public static async Task UpdateRecordByNameAndDate(long userId, string personName, DateTime date)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "UPDATE users_schedule SET bday_date = @date " +
+                "WHERE user_telegram_id = @userId AND human_in_schedule = @personName");
+            
+            AddParametersByInput(command, 1, userId, personName, date);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public static async Task RemoveRecordByName(long userId, string? personName)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "DELETE FROM users_schedule WHERE user_telegram_id = @userId AND human_in_schedule = @personName");
+
+            AddParametersByInput(command, 2, userId, personName);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public static async Task<bool> IsRecordExist(long userId, string? personName)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "SELECT COUNT(*) FROM users_schedule WHERE user_telegram_id = @userId AND human_in_schedule");
+
+            AddParametersByInput(command, 2, userId, personName);
+            var result = await command.ExecuteScalarAsync();
+            var count = Convert.ToInt32(result);
+
+            return count != 0;
+        }
+
+        public static async Task<List<PersonInDataBase>> GetData(long userId)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "SELECT id, user_telegram_id, human_in_schedule, bday_date FROM users_schedule " +
+                "WHERE user_telegram_id = @userId");
+            
+            AddParametersByInput(command, 3, userId);
+            var dataset = new List<PersonInDataBase>();
+            await using (var reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    var humanData = new PersonInDataBase
+                    {
+                        Id = reader.GetInt32(0),
+                        TelegramId = reader.GetInt64(1),
+                        Name = reader.GetString(2),
+                        BirthdayDate = reader.IsDBNull(3) 
+                            ? DateTime.MinValue 
+                            : reader.GetDateTime(3)
+                    };
+
+                    dataset.Add(humanData);
+                }
+            }
+
+            return dataset;
+        }
+
+        public static async Task RemoveAllRecords(long userId)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "DELETE FROM users_schedule WHERE user_telegram_id = @userId");
+
+            AddParametersByInput(command, 3, userId);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public static async Task RemoveInvalidRecords()
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection, 
+                                  "DELETE FROM users_schedule " +
+                                  "WHERE user_telegram_id = 0 " +
+                                  "OR human_in_schedule = 'unknown' " +
+                                  "OR bday_date = '0001-01-01';");
+
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public static async Task<bool> IsUserScheduleEmpty(long userId)
+        {
+            await using var connection = await GetOpenConnectionAsync();
+            await using var command = await GetCommandAsync(connection,
+                "SELECT COUNT(*) FROM users_schedule WHERE user_telegram_id = @userId");
+
+            AddParametersByInput(command, 3, userId);
+            var result = await command.ExecuteScalarAsync();
+            var count = Convert.ToInt32(result);
+
+            return count == 0;
+        }
+
+        private static async Task<MySqlConnection> GetOpenConnectionAsync()
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+
+            return connection;
+        }
+
+        private static async Task<MySqlCommand> GetCommandAsync(MySqlConnection connection, string commandText)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = commandText;
+
+            return await Task.FromResult(command);
+        }
+
+        public static void AddParametersByInput(MySqlCommand command, int input, long userId, string? personName = null, DateTime? date = null)
+        {
+            switch (input)
+            {
+                case 1:
+                {
+                    command.Parameters.AddWithValue("@userId", userId);
+                    command.Parameters.AddWithValue("@personName", personName);
+                    command.Parameters.AddWithValue("@date", date);
+                    break;
+                }
+                case 2:
+                {
+                    command.Parameters.AddWithValue("@userId", userId);
+                    command.Parameters.AddWithValue("@personName", personName);
+                    break;
+                }
+                case 3:
+                {
+                    command.Parameters.AddWithValue("@userId", userId);
+                    break;
+                }
+                default:
+                    throw new ArgumentException("Invalid input for adding parameters");
+            }
+        }
+    }
+}
