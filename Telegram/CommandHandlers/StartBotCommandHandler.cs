2276b310e727492070aa6787e08becbad5eb10d0
szymptom <bpodkolzin@kse.org.ua>
2024-06-17 20:33:27 +0300

    Relocate helpers methods into different file StartBotHelper.cs

--- Telegram/CommandHandlers/StartBotCommandHandler.cs
+++ Telegram/CommandHandlers/StartBotCommandHandler.cs
@@ -1,97 +1,54 @@
-﻿    using BirthdayReminder.DependencyInjectionConfiguration;
-    using BirthdayReminder.PersonReminder;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-    using Newtonsoft.Json.Linq;
-    using PRTelegramBot.Attributes;
-    using PRTelegramBot.Extensions;
-    using PRTelegramBot.Models;
-    using Telegram.Bot;
-    using Telegram.Bot.Types;
+﻿using BirthdayReminder.PersonReminder;
+using PRTelegramBot.Attributes;
+using PRTelegramBot.Extensions;
+using PRTelegramBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using static BirthdayReminder.Telegram.Helpers.StartBotHelper;
 
-    namespace BirthdayReminder.Telegram.CommandHandlers
+namespace BirthdayReminder.Telegram.CommandHandlers
+{
+    public class StartBotCommandHandler
     {
-        public class StartBotCommandHandler
+
+        [ReplyMenuHandler("/start")]
+        public static async Task StartBotMethod(ITelegramBotClient botClient, Update update)
         {
-
-            [ReplyMenuHandler("/start")]
-            public static async Task StartBotMethod(ITelegramBotClient botClient, Update update)
+            if (update.Message?.From == null)
             {
+                return;
+            }
 
-                if (update.Message?.From == null)
-                {
-                    return;
-                }
-
-                var user = update.Message.From;
-                var userNickName = user?.Username ?? "";
-
-                var startMessage = $"🖐️ Hey, @{userNickName}!\n" +
-                                   $"For the bot to work correctly, specify the city closest to you:";
+            var userTelegramTag = update.Message.From?.Username ?? "";
+            var startMessage = $"🖐️ Hey, @{userTelegramTag}!\n" +
+                                $"For the bot to work correctly, specify the city closest to you:";
                 
-                update.RegisterStepHandler(new StepTelegram(GetUserTimezone));
-                await PRTelegramBot.Helpers.Message.Send(botClient, update, startMessage);
-                await InfinityLoop.StartReminderLoop(botClient, update);
-            }
+            update.RegisterStepHandler(new StepTelegram(GetUserTimezone));
 
-            public static async Task GetUserTimezone(ITelegramBotClient botClient, Update update)
+            await PRTelegramBot.Helpers.Message.Send(botClient, update, startMessage);
+            await InfinityLoop.StartReminderLoop(botClient, update);
+        }
+
+        public static async Task GetUserTimezone(ITelegramBotClient botClient, Update update)
+        {
+            var city = update.Message?.Text;
+            if (city == null)
             {
-                var city = update.Message?.Text;
-                if (city == null)
-                {
-                    await PRTelegramBot.Helpers.Message.Send(botClient, update, "Please enter the city name");
-                    return;
-                }
-
-                var (latitude, longitude) = await GetLatitudeAndLongitude(city);
-                if (latitude == null || longitude == null)
-                {
-                    await PRTelegramBot.Helpers.Message.Send(botClient, update, "City not found");
-                    return;
-                }
-
-                var message = await GetTimezone(latitude, longitude);
-
-                update.ClearStepUserHandler();
-                await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+                await PRTelegramBot.Helpers.Message.Send(botClient, update, "Please enter the city name");
+                return;
             }
-            
-            private static async Task<(string? lat, string? lon)> GetLatitudeAndLongitude(string city)
+
+            var (latitude, longitude) = await GetLatitudeAndLongitude(city);
+            if (latitude == null || longitude == null)
             {
-                var url = $"https://nominatim.openstreetmap.org/search?q={Uri.EscapeDataString(city)}&format=json";
-                using HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
-
-                var response = await client.GetStringAsync(url);
-                var results = JArray.Parse(response);
-
-                var jsonResult = results[0];
-
-                var lat = jsonResult["lat"]?.Value<string>();
-                var lon = jsonResult["lon"]?.Value<string>();
-
-                return (lat, lon);
+                await PRTelegramBot.Helpers.Message.Send(botClient, update, "City not found");
+                return;
             }
 
-            private static async Task<string> GetTimezone(string lat, string lon)
-            {
-                var url =
-                    $"https://api.timezonedb.com/v2.1/get-time-zone?key=7TVJUMUJ9LMG&format=json&by=position&lat={lat}&lng={lon}";
-                using HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
+            var message = await GetPlaceInformation(latitude, longitude);
+            await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
 
-                var response = await client.GetStringAsync(url);
-                var results = JObject.Parse(response);
-
-                var town = results["cityName"]?.Value<string>();
-                var region = results["countryName"]?.Value<string>();
-                var time = results["formatted"]?.Value<string>();
-
-                var message =
-                    $"City: {town}, {region}\n" +
-                    $"Time: {time}";
-
-                return message;
-            }
+            update.ClearStepUserHandler();
         }
     }
+}
---
+++ Telegram/Helpers/StartBotHelper.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace BirthdayReminder.Telegram.Helpers
+{
+    public class StartBotHelper
+    {
+        private static HttpClient GetConfiguredHttpClient()
+        {
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
+            return client;
+        }
+
+        public static async Task<(string? lat, string? lon)> GetLatitudeAndLongitude(string city)
+        {
+            var url = $"https://nominatim.openstreetmap.org/search?q={Uri.EscapeDataString(city)}&format=json";
+            using var client = GetConfiguredHttpClient();
+
+            var response = await client.GetStringAsync(url);
+            var results = JArray.Parse(response);
+
+            var jsonResult = results[0];
+
+            var lat = jsonResult["lat"]?.Value<string>();
+            var lon = jsonResult["lon"]?.Value<string>();
+
+            return (lat, lon);
+        }
+
+        public static async Task<string> GetPlaceInformation(string lat, string lon)
+        {
+            var url =
+                $"https://api.timezonedb.com/v2.1/get-time-zone?key=7TVJUMUJ9LMG&format=json&by=position&lat={lat}&lng={lon}";
+            using var client = GetConfiguredHttpClient();
+
+            var response = await client.GetStringAsync(url);
+            var results = JObject.Parse(response);
+
+            var town = results["cityName"]?.Value<string>();
+            var region = results["countryName"]?.Value<string>();
+            var time = results["formatted"]?.Value<DateTime>();
+
+            var message =
+                $"City: {town}, {region}\n" +
+                $"Time: {time}";
+
+            return message;
+        }
+    }
+}
